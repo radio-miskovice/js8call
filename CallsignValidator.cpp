9029667226e46963accf6313cf04ea7add570831
Jindrich Vavruska <jindrich@vavruska.cz>
2024-11-13 12:33:07 +0100

    fixed callsign validator, added SKIP_OMNIRIG Cmake variable

--- CallsignValidator.cpp
+++ CallsignValidator.cpp
@@ -2,15 +2,17 @@
 
 CallsignValidator::CallsignValidator (QObject * parent, bool allow_compound)
   : QValidator {parent}
-  , re_ {allow_compound ? R"(^[A-Za-z0-9/]+$)" : R"(^[A-Za-z0-9]+$)"}
+  , re_ {allow_compound ? 
+      R"(^(([A-Z]{2}|\\d[A-Z]|[A-Z]\\d)/)?([A-Z]{2}|\\d[A-Z]|[A-Z]\\d)\\d+[A-Z]+(/(P|M|A|\\d|QRP|(([A-Z]{2}|\\d[A-Z]|[A-Z]\\d))))$)"
+      : R"(^([A-Z]{2}|\\d[A-Z]|[A-Z]\\d)\\d+[A-Z]+$)"}
 {
 }
 
 auto CallsignValidator::validate (QString& input, int& pos) const -> State
 {
+  re_.setPatternOptions( QRegularExpression::CaseInsensitiveOption );
   auto match = re_.match (input, 0, QRegularExpression::PartialPreferCompleteMatch);
   input = input.toUpper ();
-  if (input.count(QLatin1Char('/')) > 2) return Invalid;
   if (match.hasMatch ()) return Acceptable;
   if (!input.size () || match.hasPartialMatch ()) return Intermediate;
   pos = input.size ();
